2df1d7d52c57a086e6b46b7c12c46dfdbb4089d9
sk1ppi <kcxyzxyz@icloud.com>
2024-02-02 06:58:55 +0800

    Refactor getLatestTweetsByUsername function and update test cases

--- index.test.js
+++ index.test.js
@@ -1,24 +1,42 @@
-const { getLatestTweetsByUsername } = require('./index')
+const https = require("https");
+const { getLatestTweetsByUsername } = require("./index")
 
-describe('getLatestTweetsByUsername', () => {
+describe("getLatestTweetsByUsername", () => {
+    const agent = new https.Agent({
+        rejectUnauthorized: false
+    });
 
-    it('throws an error if no username is provided', async () => {
-        await expect(getLatestTweetsByUsername()).rejects.toThrow('Twitter username is required')
+    it("throws an error if no username is provided", async () => {
+        await expect(getLatestTweetsByUsername()).rejects.toThrow("Twitter username is required")
     })
 
-    it('returns an array of tweets', async () => {
-        const tweets = await getLatestTweetsByUsername('elonmusk')
+    it("returns an array of tweets", async () => {
+        const tweets = await getLatestTweetsByUsername("elonmusk", {
+            httpsAgent: agent
+        })
         expect(tweets).toBeInstanceOf(Array)
     })
 
-    it('returns an array of tweets with the correct properties', async () => {
-        const properties = ['tweetLink', 'tweetPhotoURL', 'tweetFullName', 'tweetUsername', 'tweetDate', 'tweetMoment', 'tweetText', 'tweetTextHtml', 'tweetFullHtml', 'tweetId']
-        const tweets = await getLatestTweetsByUsername('elonmusk')
+    it("returns an array of tweets with the correct properties", async () => {
+        const properties = [
+            "fullName",
+            "username",
+            "id",
+            "date",
+            "photoUrl",
+            "contentHtml",
+            "content",
+            "dateMoment",
+            "link"
+        ]
+        const tweets = await getLatestTweetsByUsername("elonmusk", {
+            httpsAgent: agent
+        })
         const tweet = tweets[0]
         const tweetProperties = Object.keys(tweet)
 
         // attachments is optional
-        const hasAttachments = tweetProperties.includes('tweetAttachments')
+        const hasAttachments = tweetProperties.includes("attachments")
 
         expect(tweet).toBeInstanceOf(Object)
 
@@ -27,7 +45,7 @@ describe('getLatestTweetsByUsername', () => {
         })
 
         if (hasAttachments) {
-            expect(tweetProperties).toContain('tweetAttachments')
+            expect(tweetProperties).toContain("attachments")
         }
     })
 })
