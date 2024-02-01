854e730c413f1d2743ed8b9b7a516c6a73192ea8
sk1ppi <kcxyzxyz@icloud.com>
2024-02-02 06:58:50 +0800

    Refactor code to use consistent quotation marks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
-const moment = require('moment')
+const axios = require("axios")
+const cheerio = require("cheerio")
+const moment = require("moment")
 
 /**
  * Retrieves the latest tweets by username from the Nitter website.
@@ -16,58 +16,56 @@ const moment = require('moment')
  */
 async function getLatestTweetsByUsername(username, axiosConfig = {}) {
     if (!username) {
-        throw new Error('Twitter username is required')
+        throw new Error("Twitter username is required")
     }
 
     const $ = await axios({
-        method: 'GET',
+        method: "GET",
         url: `https://nitter.net/${username}`,
         headers: {
-            'Accept': 'text/html',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Cache-Control': 'no-cache',
-            'Pragma': 'no-cache',
-            'TE': 'Trailers',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0'
+            "Accept": "text/html",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Cache-Control": "no-cache",
+            "Pragma": "no-cache",
+            "TE": "Trailers",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"
         },
         ...axiosConfig
     })
         .then((response) => {
             return cheerio.load(response.data)
         })
-        .catch((error) => console.log(error))
 
     const tweets = []
-    const timelineItemCss = 'div.timeline-item'
+    const timelineItemCss = "div.timeline-item"
     const timelimeItemList = $(timelineItemCss)
 
     timelimeItemList.each((index, element) => {
 
-        const tweetLink = $(element).find('a.tweet-link').attr('href')
-        const tweetPhotoURL = $(element).find('div.tweet-body div.tweet-header a.tweet-avatar img.avatar').attr('src')
-        const tweetFullName = $(element).find('div.tweet-body div.tweet-header div.tweet-name-row div.fullname-and-username a.fullname').text()
-        const tweetUsername = $(element).find('div.tweet-body div.tweet-header div.tweet-name-row div.fullname-and-username a.username').text()
-        const tweetDate = $(element).find('div.tweet-body div.tweet-header div.tweet-name-row span.tweet-date a').attr('title')
-        const tweetId = $(element).find('div.tweet-body div.tweet-header div.tweet-name-row span.tweet-date a').attr('href').replace('/', '')
-        const tweetMoment = moment(tweetDate, 'MMM DD, YYYY · hh:mm A UTC')
-        const tweetText = $(element).find('div.tweet-body div.tweet-content').text()
-        const tweetTextHtml = $(element).find('div.tweet-body div.tweet-content').html()
-        const tweetAttachments = $(element).find('div.tweet-body div.gallery-video').html()
+        const fullName = $(element).find("div.tweet-body div.tweet-header div.tweet-name-row div.fullname-and-username a.fullname").text()
+        const username = $(element).find("div.tweet-body div.tweet-header div.tweet-name-row div.fullname-and-username a.username").text()
+        const id = $(element).find("div.tweet-body div.tweet-header div.tweet-name-row span.tweet-date a").attr("href").replace("/", "")
+        const date = $(element).find("div.tweet-body div.tweet-header div.tweet-name-row span.tweet-date a").attr("title")
+        const photoUrl = $(element).find("div.tweet-body div.tweet-header a.tweet-avatar img.avatar").attr("src")
+        const attachments = $(element).find("div.tweet-body div.gallery-video").html()
+        const contentHtml = $(element).find("div.tweet-body div.tweet-content").html()
+        const content = $(element).find("div.tweet-body div.tweet-content").text()
+        const dateMoment = moment(date, "MMM DD, YYYY · hh:mm A UTC")
+        const link = $(element).find("a.tweet-link").attr("href")
 
 
         const tweet = {
-            tweetLink,
-            tweetPhotoURL,
-            tweetFullName,
-            tweetUsername,
-            tweetDate,
-            tweetMoment,
-            tweetText,
-            tweetTextHtml,
-            tweetAttachments,
-            tweetFullHtml: $(element).html(),
-            tweetId
+            fullName,
+            username,
+            id,
+            date,
+            photoUrl,
+            attachments,
+            contentHtml,
+            content,
+            dateMoment,
+            link
         }
 
         tweets.push(tweet)
